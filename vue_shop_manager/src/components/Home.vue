98df69f2030c4d5f5e14963a70db138245b30286
honyausgt <a13975991339@gmail.com>
2019-07-06 22:39:15 +0800

    实现登录页面布局

---
+++ vue_shop_manager/src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <el-button type="info" @click="logout">退出</el-button>
+    <h1>欢迎</h1>
+
+    <el-progress :percentage="0"></el-progress>
+    <el-progress :percentage="70"></el-progress>
+    <el-progress :percentage="80" color="#8e71c7"></el-progress>
+    <el-progress :percentage="100" status="success"></el-progress>
+    <el-progress :percentage="50" status="exception"></el-progress>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout() {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
---
+++ vue_shop_manager/src/plugins/element.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import { Button, Form, FormItem, Input, Message, Progress } from 'element-ui'
+
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.use(Progress)
+Vue.prototype.$message = Message
